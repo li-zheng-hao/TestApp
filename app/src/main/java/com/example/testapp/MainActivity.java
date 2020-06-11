de523b3e62b41aa2be0c44e8bbc4746480a62e0c
LiZhengHao <1263212577@qq.com>
2020-06-11 12:25:49 +0800

    修改项目

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        int i = 1;
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
